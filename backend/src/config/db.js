3434d2b6f53d97eaa10249d484c630a769203213
Miguel Dhein <migueldhein50@gmail.com>
2026-05-22 15:05:31 -0300

    Usa DATABASE_URL para conexão MySQL

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,19 +1,12 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    dialect: "mysql",
-    logging: false,
-    dialectOptions: {
-      connectTimeout: 60000,
-    },
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "mysql",
+  logging: false,
+  dialectOptions: {
+    connectTimeout: 60000,
+  },
+});
 
 module.exports = sequelize;
