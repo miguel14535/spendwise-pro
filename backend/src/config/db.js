47a5092797f96b506dea8f160178b4232eb9ca23
Miguel Dhein <migueldhein50@gmail.com>
2026-05-22 14:47:46 -0300

    Configura porta do banco no Sequelize

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -7,8 +7,12 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
     dialect: "mysql",
     logging: false,
+    dialectOptions: {
+      connectTimeout: 60000,
+    },
   }
 );
 
