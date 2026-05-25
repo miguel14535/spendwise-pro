d7364a0d5ff0741e470587919f4ec9b4a87b13f9
Miguel Dhein <migueldhein50@gmail.com>
2026-05-25 14:02:32 -0300

    Corrige db config

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,12 +1,15 @@
 const { Sequelize } = require("sequelize");
-require("dotenv").config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "mysql",
-  logging: false,
-  dialectOptions: {
-    connectTimeout: 60000,
-  },
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+    logging: false,
+  }
+);
 
 module.exports = sequelize;
